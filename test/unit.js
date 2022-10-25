40b51ab0cc31a5cd2f7c5252a94cbbdc32bbd153
Garfonso <garfonso@mobo.info>
2022-10-25 09:38:10 +0200

    get rid of gulp and new release script

--- test/unit.js
+++ test/unit.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const { tests } = require('@iobroker/testing');
 
-const sinon_1 = require("sinon");
+const sinon_1 = require('sinon');
 
 // Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
 tests.unit(path.join(__dirname, '..'), {
